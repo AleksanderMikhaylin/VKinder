c1ef677caa9998f3584df1ad95bed1576b6537e4
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-02-16 09:03:29 +1100

    Update message_sender.py

--- handler/helper/message_sender.py
+++ handler/helper/message_sender.py
@@ -1,4 +1,5 @@
 from random import randrange
+from VKinder.settings import KEYBOARD_EMPTY
 
 class MessageSender:
 
@@ -6,5 +7,16 @@ class MessageSender:
         self.vk_group_client = vk_group_client
         self.user_id = user_id
 
-    def write_msg(self, message):
-        self.vk_group_client.method('messages.send', {'user_id': self.user_id, 'message': message, 'random_id': randrange(10 ** 7)})
+    def write_msg(self, message, keyboard=None):
+
+        if keyboard is None:
+            keyboard = KEYBOARD_EMPTY
+
+        param = {
+            'user_id': self.user_id,
+            'message': message,
+            'random_id': randrange(10 ** 7),
+            'keyboard': keyboard,
+        }
+
+        self.vk_group_client.method('messages.send', param)
