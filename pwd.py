703809fc2fb5fed4210462988e07f38b8afcfb75
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-02-16 08:59:26 +1100

    Update pwd.py

--- pwd.py
+++ pwd.py
@@ -1,7 +1,4 @@
-POSTGRES_SECRET = {
-    'user': 'postgres',
-    'pass': 'postgres',
-}
 
-GROUP_TOKEN = 'vk1.a.-BgcYz7YVrn3gAwI...'
-NAME_DB = 'vk_api_db'
+USER = 'postgres'
+PASSWORD = 'postgres'
+GROUP_TOKEN = 'vk1.a.-BgcYz7YVrn3gAwItstQ7E4Nb08TtvDb...'
