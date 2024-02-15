58efa9b24dacf52ac8bf653b24c4b8513f6a9386
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2024-02-16 08:59:46 +1100

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -1,16 +1,22 @@
-from VKinder.handler.handlers import FallbackHandler, FarewellHandler, GreetingsHandler
-from VKinder.handler.search_handler import SearchHandler
+from VKinder.handler.handlers import FallbackHandler, FarewellHandler, GreetingsHandler, SearchHandler, CandidatsHandler
 from VKinder.model.tables import User
+from handler.question.questions import TokenQuestion
 
 class VkBot:
 
     def __init__(self, user_id, vk_group_client, db_session):
         self.user_id = user_id
         self.db_session = db_session
-        self.handlers = [
+        self.first_handlers = [
             GreetingsHandler(user_id, vk_group_client),
             FarewellHandler(user_id, vk_group_client),
+        ]
+        self.handlers = [
+            TokenQuestion(user_id, vk_group_client, db_session),
             SearchHandler(user_id, vk_group_client, db_session),
+            CandidatsHandler(user_id, vk_group_client, db_session),
+            # GreetingsHandler(user_id, vk_group_client),
+            # FarewellHandler(user_id, vk_group_client),
             FallbackHandler(user_id, vk_group_client),
         ]
         self.last_handler = None
@@ -20,16 +26,35 @@ class VkBot:
         exists = self.db_session.query(User).filter(User.id == self.user_id).first()
         if not exists:
             self.db_session.add(User(id=self.user_id))
+            self.db_session.commit()
 
     def handle_new_message(self, message):
+        for handler in self.first_handlers:
+            handler_message = handler.handle(message)
+            if handler_message.get('status', True):
+                # self.last_handler = handler
+                for handler in self.handlers:
+                    if hasattr(handler, 'candidates') :
+                        handler.reset()
+                return
+
         self_last_handler = self.last_handler
         if self_last_handler is not None:
             self_last_handler_is_active = self.last_handler.is_active()
             if self_last_handler_is_active:
-                self.last_handler.handle(message)
-                return
+                last_handler_message = self.last_handler.handle(message)
+                if last_handler_message.get('status', True):
+                    # self.last_handler = None
+                    return
+                message = last_handler_message.get('message', '')
 
         for handler in self.handlers:
-            if handler.handle(message):
+            if hasattr(handler, 'candidates') and hasattr(self.last_handler, 'candidates'):
+                handler.candidates = self.last_handler.candidates.copy()
+            handler_message = handler.handle(message)
+            if handler_message.get('status', True):
                 self.last_handler = handler
                 break
+            if hasattr(handler, 'candidates'):
+                self.last_handler = handler
+            message = handler_message.get('message', '')
